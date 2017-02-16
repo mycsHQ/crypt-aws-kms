ebf750e78fbda2529f114723d24c3598d31e8845
johann <johann@mycs.com>
2017-02-16 17:47:46 +0100

    removed some clutter

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const
-  fs = require('fs'),
+  // fs = require('fs'),
   AWS = require('aws-sdk');
 
 const kms = new AWS.KMS({
   region: 'eu-west-1'
 });
 
-const secretPath = './encrypted-secret';
-const encryptedSecret = fs.readFileSync(secretPath);
+/* aws kms encrypt
+ * --key-id 6bda8121-95b4-402a-83c3-26ae49e8a9d8
+ * --plaintext "This is the secret you want to encrypt"
+ * --query CiphertextBlob --output text | base64 -D > ./encrypted-secret
+ */
+// const secretPath = './encrypted-secret';
+// const encryptedSecret = fs.readFileSync(secretPath);
+
 
 const decrypt = (encryptedData) => {
+  // http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/KMS.html#decrypt-property
   kms.decrypt(encryptedData, (err, data) => {
     if (err) {
       console.error(err, err.stack);
@@ -27,11 +34,7 @@ const encryptParams = {
   Plaintext: 'This is so encrypted'
 };
 
-/* aws kms encrypt
- * --key-id 6bda8121-95b4-402a-83c3-26ae49e8a9d8
- * --plaintext "This is the secret you want to encrypt"
- * --query CiphertextBlob --output text | base64 -D > ./encrypted-secret
- */
+// http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/KMS.html#encrypt-property
 kms.encrypt(encryptParams, (err, data) => {
   if (err) {
     console.log(err, err.stack);
