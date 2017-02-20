fd7c2b10b9a0030e410f99ae4cbd1ad2c1c72b8a
johann <johann@mycs.com>
2017-02-20 11:52:38 +0100

    test refinements

--- specs/KMS.spec.js
+++ specs/KMS.spec.js
@@ -2,47 +2,49 @@ const
   fs = require('fs'),
   KMS = require('../lib/KMS');
 
-const testDir = './test/tmp';
 const KeyId = '6bda8121-95b4-402a-83c3-26ae49e8a9d8';
+
+let encryptedFile;
 let encryptedData;
 
-describe('nscabinet:', () => {
-  beforeAll(() => {
-    !fs.existsSync(testDir) && fs.mkdirSync(testDir);
-  });
-
-  afterAll(() => {
-    fs.rmdirSync(testDir);
-  });
-
-  it('encrypt file', () => {
+describe('KMS:', () => {
+  it('encrypts file', () => {
     const kms = new KMS(KeyId);
     return kms.encryptFile('./test/encrypt-this.txt').then(res => {
-      expect(res).toBeDefined();
-      expect(fs.existsSync('./test/encr_0')).toBe(true);
+      const {
+        CiphertextBlob,
+        file
+      } = res;
+      expect(CiphertextBlob).toBeDefined();
+      encryptedFile = file;
+      expect(fs.existsSync(file)).toBe(true);
     });
   });
 
-  it('decrypt file', () => {
+  it('decrypts file', () => {
     const kms = new KMS();
     return kms.decryptFile('./test/encr_0').then(res => {
       expect(res.Plaintext.toString()).toBe('This is the secret you want to encrypt');
-      fs.unlinkSync('./test/encr_0');
+      fs.unlinkSync(encryptedFile);
     });
   });
 
-  it('encrypt data', () => {
+  it('encrypts data', () => {
     const kms = new KMS(KeyId);
     return kms.encryptData('Encrypt this please').then(res => {
-      encryptedData = res;
-      expect(res).toBeDefined();
-      expect(fs.existsSync('./encr_0')).toBe(true);
-      fs.unlinkSync('./encr_0');
+      const {
+        CiphertextBlob,
+        file
+      } = res;
+      encryptedData = CiphertextBlob;
+      expect(CiphertextBlob).toBeDefined();
+      expect(fs.existsSync(file)).toBe(true);
+      fs.unlinkSync(file);
     });
   });
 
-  it('decrypt data', () => {
-    const kms = new KMS(KeyId);
+  it('decrypts data', () => {
+    const kms = new KMS();
     return kms.decryptData(encryptedData).then(res => {
       expect(res.Plaintext.toString()).toBe('Encrypt this please');
     });
